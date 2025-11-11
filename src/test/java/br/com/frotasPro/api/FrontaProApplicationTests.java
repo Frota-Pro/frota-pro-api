733013f636fd96aa02ed631a2395922b3de64458
Alexandre Gonçalves Evangelista <alexandre-evangelista@hotmail.com>
2025-11-11 15:40:24 -0300

    iniciando projeto com configuracoes iniciais de pasta

---
+++ src/test/java/br/com/frotasPro/api/FrontaProApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.frotasPro.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontaProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
